c2e9f04c62b736b9b8c55d9c7124fc0698c1a436
aleviannaf <alexandrevianna511@gmail.com>
2023-12-14 12:24:04 -0400

    feat: handle readble and writable  stream

---
+++ stream/fundamentos.js
@@ -0,0 +1,36 @@
+// streams ->
+
+/* process.stdin
+.pipe(process.stdout) */
+
+//contruindo stream do total zero
+
+import { Readable, Writable } from "node:stream"
+
+class OneToHundredStream extends Readable {
+    index  = 1
+
+    _read() {
+        const i = this.index++
+
+        setTimeout(()=>{
+            if(i > 100) {
+                this.push(null)
+            } else {
+    
+                const buf = Buffer.from(String(i))
+                this.push(buf)
+            }
+        }, 1000)
+    }
+}
+
+class MultiplyByTenSteam extends Writable{
+    _write(chunk, encoding, callback){
+        console.log(Number(chunk.toString() * 10))
+        callback()
+    }
+}
+
+new OneToHundredStream()
+.pipe(new MultiplyByTenSteam())

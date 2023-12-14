d2461d0095ff2d01906fd3b94cb3ed6d853bd31c
aleviannaf <alexandrevianna511@gmail.com>
2023-12-14 16:38:59 -0400

    feat: buffer

---
+++ stream/buffer.js
@@ -0,0 +1,6 @@
+const buf = Buffer.from("Alexandre")
+//É uma forma que o node encontrou de trabalha de forma nativa com dados binários
+// O buff é uma forma de trabalhar com binaria em um hexadecimal ou etc
+
+console.log(buf)
+console.log(buf.toJSON())
